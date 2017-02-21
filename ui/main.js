fb002071aedb41394fdab8c98a25fc6631abff52
kamblemayur <kamblemayur2811@gmail.com>
2017-02-21 16:39:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+//code for counter
+var buttton=document.getElementById("counter");
+var counter=0;
+butoon.onClick=function(){
+    
+//first make request to
+counter=counter+1;
+var span=document.getElementById("count")
+span.innerHTML=counter.toString();
+    
+}
